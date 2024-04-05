84b77e9fb45d23c0a08a92150961b377b3346eb3
piyashah123 <82143407+piyashah123@users.noreply.github.com>
2024-04-05 14:57:12 +0530

    Add files via upload

---
+++ components/Rotation.js
@@ -0,0 +1,49 @@
+AFRAME.registerComponent("scuba-diver", {
+  init: function () {
+    this.el.setAttribute('gltf-model', '#scuba-diver-model');
+  }
+
+
+.const,scubaDiverModel = `
+{
+  "scene": {
+    "nodes": [
+      {
+        "name": "scuba-diver",
+        "mesh": 1,
+        "skin": 0,
+        "primitive": {
+          "type": "SkinnedMesh"
+        },
+        "material": 0,
+        "animation": [
+          {
+            "name": "Armature",
+            "fps": 25,
+            "tracks": [
+              {
+                "name": "Hips",
+                "node": 0,
+                "tracks": [
+                  {
+                    "name": "translation",
+                    "times": [0, 0.1],
+                    "values": [0, 0, 0],
+                    "easing": "linear"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
+`,
+
+AFRAME.registerAsset("scuba-diver-model", {
+  type: 'json',
+  value: scubaDiverModel
+}),
+});
